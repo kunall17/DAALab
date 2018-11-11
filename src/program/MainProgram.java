6c072ed2785e1270ac16fb6cafb55f98c6f9dc1c
aaadilmaq0 <aaadilmaq0@gmail.com>
2018-11-11 15:44:59 +0530

    made the jar

---
+++ src/program/MainProgram.java
@@ -0,0 +1,58 @@
+package program;
+
+import java.io.IOException;
+
+import jdk.nashorn.internal.scripts.JO;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class MainProgram {
+    public static void main(String args[])throws Exception{
+        Configuration c=new Configuration();
+        String[] files=new GenericOptionsParser(c,args).getRemainingArgs();
+        Path input=new Path(files[0]),output=new Path(files[1]);
+        Job j=new Job(c,"group");
+        j.setJarByClass(MainProgram.class);
+        j.setMapperClass(MapForGroup.class);
+        j.setReducerClass(ReduceForGroup.class);
+        j.setOutputKeyClass(Text.class);
+        j.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(j,input);
+        FileOutputFormat.setOutputPath(j,output);
+        System.exit(j.waitForCompletion(true)?0:1);
+    }
+    public static class MapForGroup extends Mapper<Text, Text, Text, Text>{
+        @Override
+        public void map(Text key,Text value, Context context)throws IOException, InterruptedException{
+            String line=value.toString();
+            String words[]=line.split(",");
+            int l=words.length;
+            Text outputValue=new Text(words[0]);
+            if(l>1){
+                for(int i=1;i<l;i++){
+                    Text outputKey=new Text(words[i]);
+                    context.write(outputKey,outputValue);
+                }
+            }
+        }
+    }
+    public static class ReduceForGroup extends Reducer<Text, Text, Text, Text>{
+        @Override
+        public void reduce(Text word, Iterable<Text> values,Context context) throws IOException, InterruptedException{
+            String allValues="";
+            for(Text value : values){
+                allValues+=","+value.toString();
+            }
+            context.write(word,new Text(allValues));
+        }
+    }
+}
